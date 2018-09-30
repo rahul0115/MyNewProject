4c0764706a07a9a1bb4ab8581f24082741fed312
alok <alok@alok-PC>
2018-09-30 11:50:19 +0530

    First Commit

---
+++ src/com/simple/Test.java
@@ -0,0 +1,11 @@
+package com.simple;
+
+public class Test {
+
+	public static void main(String[] args) {
+		String s="Alok Ranjan";
+		String s1="Ji Good Morning";
+		System.out.println("Hello :"+s + " "+s1);
+	}
+
+}
